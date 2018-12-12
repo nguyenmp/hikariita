7dbc288a990f7454e3b251323c83ef045aa17fb2
Mark Nguyen <mnguyen4@apple.com>
2018-12-11 16:59:54 -0800

    Created base database and ability to look at a random card

--- hikariita/__init__.py
+++ hikariita/__init__.py
@@ -6,23 +6,85 @@ This module is an application for flashcarding Japanese.
 
 from __future__ import unicode_literals
 
+import sqlite3
+
 from flask import (
     Flask,
     render_template,
+    redirect,
+    url_for,
+    g,
 )
+
+from . import db
 
 
 APP = Flask(__name__)
 
 
-@APP.route('/')
-def card():
+def get_db():
+    '''
+    Returns the cached database connection
+    '''
+    if 'db' not in g:
+        g.db = sqlite3.connect('example.db')
+    return g.db
+
+
+
+@APP.route('/', methods=['GET'])
+def index():
+    '''
+    Home page
+    '''
+    return "Welcome!"
+
+
+@APP.route('/cards/', methods=['GET'])
+def cards():
+    '''
+    The index of cards, redirects to an instance of a random card
+    '''
+    card_id = db.get_random_card_id(get_db().cursor())
+    APP.logger.debug('Type 1: %s', type(card_id))
+    return redirect(url_for('card', card_id=card_id))
+
+
+@APP.route('/cards/<string:card_id>/', methods=['GET'])
+def card(card_id):
     '''
     Renders a single flash-card on screen
     '''
+    APP.logger.debug('Type: %s', type(card_id))
+    cursor = get_db().cursor()
+    attributes = db.get_card_attributes(cursor, card_id)
+    kanji = None
+    hiragana = None
+    meaning = None
+    others = []
+
+    for attribute in attributes:
+        if attribute[1] == 'kanji':
+            kanji = attribute[2]
+        elif attribute[1] == 'hiragana':
+            hiragana = attribute[2]
+        elif attribute[1] == 'meaning':
+            meaning = attribute[2]
+        else:
+            others.append((attribute[1], attribute[2]))
     return render_template(
         'card.html',
-        kanji="紹介",
-        hiragana="しょうかい",
-        meaning='introduction',
+        kanji=kanji,
+        hiragana=hiragana,
+        meaning=meaning,
+        others=others,
     )
+
+
+@APP.route('/cards/<int:card_id>/vote', methods=['POST'])
+def vote(card_id):
+    '''
+    Gives a vote to the given card and redirects to the next card
+    '''
+    # TODO: Set vote for card
+    return redirect(url_for('cards'))
---
+++ hikariita/db.py
@@ -0,0 +1,142 @@
+'''
+Imports flashcards into a sqlite database from CSV
+'''
+
+from __future__ import unicode_literals
+
+
+INIT_DB_COMMANDS = '''
+CREATE TABLE IF NOT EXISTS cards (id INTEGER PRIMARY KEY);
+
+CREATE TABLE IF NOT EXISTS votes (
+    id INTEGER PRIMARY KEY,
+    vote INTEGER NOT NULL,
+    card_id INTEGER NOT NULL,
+    FOREIGN KEY(card_id) REFERENCES cards(id)
+);
+
+CREATE TABLE IF NOT EXISTS attributes (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    value TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS attributes_cards_relation (
+    card_id INTEGER,
+    attribute_id INTEGER,
+    FOREIGN KEY(card_id) REFERENCES cards(id),
+    FOREIGN KEY(attribute_id) REFERENCES attributes(id)
+);
+'''
+
+
+def init(cursor):
+    '''
+    Initializes the database with the base tables
+    '''
+    cursor.executescript(INIT_DB_COMMANDS)
+
+
+def create_book(cursor, title):
+    '''
+    Adds a new book to the DB, and returns it's ID
+    '''
+    return create_attribute(cursor, "Book", title)
+
+
+def create_lesson(cursor, number):
+    '''
+    Creates a new lesson for the given number
+    '''
+    return create_attribute(cursor, "Lesson", number)
+
+
+def create_attribute(cursor, name, value):
+    '''
+    Creates a generic attribute, not associated with anything yet
+    '''
+    command = 'INSERT INTO attributes (name, value) VALUES (?, ?)'
+    cursor.execute(command, (name, value))
+    return cursor.lastrowid
+
+
+def associate_card_and_attribute(cursor, card_id, attribute_id):
+    '''
+    Attaches the given attribute to the given card
+    '''
+    command = 'INSERT INTO attributes_cards_relation (card_id, attribute_id) VALUES (?, ?)'
+    cursor.execute(command, (card_id, attribute_id))
+
+
+def create_card(cursor):
+    '''
+    Creates a blank card to fill in with attributes later
+    '''
+    command = 'INSERT INTO cards (id) VALUES (NULL)'
+    cursor.execute(command)
+    return cursor.lastrowid
+
+
+def read_data():
+    '''
+    Imports data from file system
+    '''
+    with open("~/Downloads/An Integrated Approach to Intermediate Japanese - Lesson 1.tsv", 'r') as handle:
+        content = handle.read().decode('utf8')
+
+    result = []
+
+    for line in content.splitlines():
+        parts = line.split('\t')
+        result.append(parts)
+
+    return result
+
+
+def get_random_card_id(cursor):
+    '''
+    Pick a random card and returns that card_id
+    '''
+    command = 'SELECT id FROM cards ORDER BY RANDOM() LIMIT 1'
+    cursor.execute(command)
+    row = cursor.fetchone()
+    return row[0]
+
+
+def get_card_attributes(cursor, card_id):
+    '''
+    Gets all the attributes associated with a card_id
+    '''
+    command = '''
+    SELECT * FROM attributes
+    INNER JOIN attributes_cards_relation ON attributes.id == attributes_cards_relation.attribute_id
+    WHERE attributes_cards_relation.card_id == ?
+    '''
+    cursor.execute(command, (card_id,))
+    rows = cursor.fetchall()
+    return rows
+
+
+def main():
+    ''' main '''
+    connection = sqlite3.connect('example.db')
+    cursor = connection.cursor()
+    init(cursor)
+    title = 'An Integrated Approach to Intermediate Japanese'
+    book_id = create_book(cursor, title)
+    lesson_id = create_lesson(cursor, 1)
+    for (kanji, hiragana, meaning) in read_data():
+        card_id = create_card(cursor)
+        kanji_id = create_attribute(cursor, "kanji", kanji)
+        associate_card_and_attribute(cursor, card_id, kanji_id)
+        hiragana_id = create_attribute(cursor, "hiragana", hiragana)
+        associate_card_and_attribute(cursor, card_id, hiragana_id)
+        meaning_id = create_attribute(cursor, "meaning", meaning)
+        associate_card_and_attribute(cursor, card_id, meaning_id)
+        associate_card_and_attribute(cursor, card_id, book_id)
+        associate_card_and_attribute(cursor, card_id, lesson_id)
+        connection.commit()
+
+
+if __name__ == '__main__':
+    main()
